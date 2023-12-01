6f4eb8d1498f1190d5903d1d1ac7ea774b0645e5
Eecodev <mnasser.bbk@gmail.com>
2023-12-01 17:26:17 +0100

    json file and relative data added

---
+++ server.php
@@ -0,0 +1,16 @@
+<?php
+
+// leggo il file todo-list.json e lo metto in una variabile come stringa
+$filecontent = file_get_contents("todo-list.json");
+
+// var_dump($filecontent);
+
+// decodifico la stringa in un array php
+$list = json_decode($filecontent, true);
+
+
+header('Content-Type: application/json');
+
+// stampo la lista in json
+echo json_encode($list);
+?>
