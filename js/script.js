55a0abb645a5ffb0119b63ba371004fe37a40497
Eecodev <mnasser.bbk@gmail.com>
2023-12-09 16:16:47 +0100

    updateTask function created

--- js/script.js
+++ js/script.js
@@ -24,8 +24,34 @@ createApp({
                 .finally(() => {
                   // always executed
                 });
-      
+
+
+                if(this.newTask == ""){
+                  return;
+                }
+                const data = new FormData();
+                data.append("task", this.newTask);
+
+                axios
+                .post(this.apiUrl, data)
+                .then((response) => {
+                  console.log(response.data);
+                  this.todoList = response.data;
+                })
+                .catch((error) => {
+                  console.log(error);
+                })
           },
+          updateTask(index){
+            const data = new FormData();
+            data.append("updatetask", index);
+            axios
+            .post(this.apiUrl, data)
+            .then((response) => {
+                console.log(response.data);
+                this.todoList = response.data;
+              })
+          }
     },
     mounted() {
         this.readList();
--- server.php
+++ server.php
@@ -8,9 +8,25 @@ $filecontent = file_get_contents("todo-list.json");
 // decodifico la stringa in un array php
 $list = json_decode($filecontent, true);
 
+if(isset($_POST['task'])){
+    $newtask = 
+    [
+        'text'=> $_POST['task'],
+        'done'=> false
+    ];
+    array_push($list, $newtask);
+    file_put_contents('todo-list.json', json_encode($list));
+}
+//var_dump($list);
+
+
+
+
+// $list[] = 'Vue';
+// $newContent = json_encode($list);
+// file_put_contents('todo-list.json', $newContent);
 
 header('Content-Type: application/json');
-
 // stampo la lista in json
 echo json_encode($list);
 ?>
